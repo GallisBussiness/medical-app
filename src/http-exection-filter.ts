ffb322c4e999d43841d565dbbd85a1f7f531f038
Gallis <bussinessgallis@gmail.com>
2022-12-05 14:17:41 +0000

    add port

--- src/http-exection-filter.ts
+++ src/http-exection-filter.ts
@@ -7,7 +7,6 @@ const logger = new Logger('HttpException')
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
